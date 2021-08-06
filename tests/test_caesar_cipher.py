fa5c12f85d08777e0680443c7a9b9c22992f6797
Marie <marie.marcos25@gmail.com>
2021-08-05 20:55:37 -0700

    caesar cipher

--- tests/test_caesar_cipher.py
+++ tests/test_caesar_cipher.py
@@ -1,5 +1,28 @@
-from caesar_cipher import __version__
+import pytest
 
+from caesar_cipher.caesar_cipher import encrypt, decrypt, crack
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_encrypt():
+    encrypted_word = encrypt('abc', 1)
+    assert encrypted_word == 'bcd'
+
+def test_encrypt_with_int():
+    encrypted_word = encrypt('ab124c', 1)
+    assert encrypted_word == 'bcd'
+
+def test_encrypt_with_int_and_whitespace():
+    encrypted_word = encrypt('ab1 24c', 1)
+    assert encrypted_word == 'bcd'
+
+def test_decrypt():
+    decrypted_word = decrypt('bcd', 1)
+    assert decrypted_word == 'abc'
+
+def test_decrypt_upper():
+    decrypted_word = decrypt('BCD', 1)
+    assert decrypted_word =='ABC'
+    
+def test_crack():
+    decrypted_word = crack('ifmmp')
+    assert decrypted_word == 'hello'
+
